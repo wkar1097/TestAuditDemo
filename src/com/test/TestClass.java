b7bb96bd429bd7cb7c4abcff3785d7d69d0f8e95
wiranjik <Karunarathna.Wiranji@corp.sysco.com>
2018-03-25 23:02:43 +0530

    2nd commit

---
+++ src/com/test/TestClass.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class TestClass {
+    public static void main(String[] args) {
+        String one = "One";
+        String two = "two";
+        String three = "Three";
+        String password = "SECRET";
+        String passw = "SECRET";
+
+        System.out.println("AAA");
+    }
+}
